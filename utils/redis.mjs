e78a96d647cad4ecabce28c6453ca15126da4a97
IlahaBashirova <11861@holbertonstudents.com>
2026-01-16 16:43:28 +0400

    done

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -2,15 +2,16 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient{
-    constructor() {
-        this.client = redis.createClient()
-        this.client.on('error', (err) => {
-        console.log(err);
-        });
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setexAsync = promisify(this.client.setex).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
-    }
+  constructor() {
+    this.client = redis.createClient()
+    this.client.on('error', (err) => {
+      console.log(err);
+    });
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
     isAlive() {
         return this.client.connected;
     }
