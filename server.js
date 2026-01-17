0f2d8f3710f9e272f9a168c2dbdde2f1aa942b4d
IlahaBashirova <11861@holbertonstudents.com>
2026-01-17 18:28:44 +0400

    done

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Load all routes
+routes(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
